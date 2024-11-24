b013829f8b450dbae2b87e2ef5987206d40b2386
yhkithub <129493057+yhkithub@users.noreply.github.com>
2024-11-24 10:41:25 +0900

    Update main.py

--- src/app/main.py
+++ src/app/main.py
@@ -8,7 +8,7 @@ import torchaudio.transforms as T
 from src.core.services.chatbot_service import ChatbotService
 from src.app.config import OpenAIConfig
 from src.utils.audio_handler import process_audio_input
-from src.components.message_display import apply_chat_styles, display_message
+from src.components.message_display import apply_chat_styles, display_message, get_emotion_color
 
 # 음성 감정 인식 모델 설정
 model_name = "forwarder1121/ast-finetuned-model"
