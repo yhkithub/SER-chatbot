b7879aa95362f0ac5ee3a74ce59570bbeda4ebc3
yhkithub <129493057+yhkithub@users.noreply.github.com>
2024-11-28 16:11:33 +0900

    Update chatbot_service.py

--- src/core/services/chatbot_service.py
+++ src/core/services/chatbot_service.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from typing import Dict
 from transformers import pipeline
 from langchain_openai import ChatOpenAI
-from src.core.data.personas import PERSONAS
+from src.core.services import PERSONAS
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
